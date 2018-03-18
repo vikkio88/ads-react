f0202ce0c6b3583c9c37074e8525a7fd1fb5676d
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-03-18 16:45:25 +0000

    moving models to directory

--- src/const/const.js
+++ src/const/const.js
@@ -1,6 +1,12 @@
 export const DATE_FORMAT = 'DD-MM-YYYY';
+export const YEAR = 'YYYY';
 export const MONTH_SHORT = 'MMM';
 export const DATE_FORMAT_GAP = 'YYYYMMDD';
+
 export const TEAM_NUMBER = 18;
 
 export const CURRENCY_MODIFIERS = {MILLIONS: 1000000, THOUSANDS: 1000};
+
+export const BASE_DATES = {
+    GAME_START: '01-06-'
+};
