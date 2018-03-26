7bac310a9917e7e8c6144832e2f481f9cec1e3ea
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-03-26 21:36:48 +0100

    formatting single News as card

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {Card} from "semantic-ui-react";
+import {randomizer} from "../../libs/generators";
 
 class NewsView extends Component {
     render() {
         const {news} = this.props;
-        console.log(news);
         return (
             <Card fluid>
                 <Card.Content>
-                    <Card.Header>{news.title}</Card.Header>
+                    <Card.Header
+                        style={{fontWeight: 'bold', color: randomizer.pickOne(['orange', 'red', 'blue'])}}
+                    >
+                        {news.newspaper}
+                    </Card.Header>
+                    <Card.Header><h1>{news.title}</h1></Card.Header>
+                </Card.Content>
+                <Card.Content>
                     <Card.Meta>{news.date}</Card.Meta>
                     <Card.Description>{news.message}</Card.Description>
                 </Card.Content>
