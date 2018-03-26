114690b6dae751a4eae381188b813eca6b5e82ad
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-03-26 20:18:59 +0100

    formatting single News as card

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import {Card} from "semantic-ui-react";
 
 class NewsView extends Component {
     render() {
         const {news} = this.props;
-        return (<h1>{news.title}</h1>);
+        console.log(news);
+        return (
+            <Card fluid>
+                <Card.Content>
+                    <Card.Header>{news.title}</Card.Header>
+                    <Card.Meta>{news.date}</Card.Meta>
+                    <Card.Description>{news.message}</Card.Description>
+                </Card.Content>
+            </Card>
+        );
     }
 }
 
