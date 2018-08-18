05d257285193302b0dfa02ac0c60c4f0958371af
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-08-18 16:54:28 +0100

    adding sorting for player in roster

--- src/components/player/list/SimpleList.js
+++ src/components/player/list/SimpleList.js
@@ -7,18 +7,41 @@ import {navigatePush} from "../../../store/actions";
 import {AdsFlag} from "../../common";
 
 class SimpleListView extends Component {
-    state = {
-        filter: ''
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            filter: '',
+            field: null,
+            direction: null,
+            roster: this.props.roster
+        }
+    }
+
+    handleSort(clickedColumn) {
+        let {roster, field, direction} = this.state;
+        if (field !== clickedColumn) {
+            roster = roster.sort((p, p2) => p[clickedColumn] >= p2[clickedColumn] ? 1 : -1);
+            this.setState({
+                roster,
+                field: clickedColumn,
+                direction: 'ascending'
+            });
+        } else {
+            this.setState({
+                roster: roster.reverse(),
+                direction: direction === 'ascending' ? 'descending' : 'ascending'
+            })
+        }
+
+    }
 
     getFilteredRoster() {
-        const {roster} = this.props;
-        const {filter} = this.state;
+        const {filter, roster} = this.state;
         return !filter ? roster : roster.filter(t => t.surname.toLowerCase().indexOf(filter.toLowerCase()) > -1);
     }
 
     render() {
-        const {roster} = this.props;
+        const {roster, field, direction} = this.state;
         if (!roster.length) {
             return <div/>;
         }
@@ -32,15 +55,30 @@ class SimpleListView extends Component {
                     placeholder='Filter Players by Surname...'
                     onChange={e => this.setState({filter: e.target.value})}
                 />
-                <Table celled padded unstackable>
+                <Table celled padded unstackable sortable>
                     <Table.Header>
                         <Table.Row>
                             <Table.HeaderCell>#</Table.HeaderCell>
-                            <Table.HeaderCell>Name</Table.HeaderCell>
+                            <Table.HeaderCell
+                                sorted={field === 'surname' ? direction : null}
+                                onClick={() => this.handleSort('surname')}
+                            >
+                                Name
+                            </Table.HeaderCell>
                             <Table.HeaderCell>Nationality</Table.HeaderCell>
-                            <Table.HeaderCell>Age</Table.HeaderCell>
+                            <Table.HeaderCell
+                                sorted={field === 'age' ? direction : null}
+                                onClick={() => this.handleSort('age')}
+                            >
+                                Age
+                            </Table.HeaderCell>
                             <Table.HeaderCell>Position</Table.HeaderCell>
-                            <Table.HeaderCell>Skill</Table.HeaderCell>
+                            <Table.HeaderCell
+                                sorted={field === 'skill' ? direction : null}
+                                onClick={() => this.handleSort('skill')}
+                            >
+                                Skill
+                            </Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
