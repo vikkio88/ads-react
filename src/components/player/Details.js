40589b4805e71682a3cb863536817ddbaff4753d
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-08-19 21:46:23 +0100

    improved player detail ui

--- src/components/player/Details.js
+++ src/components/player/Details.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import {Container, Flag, Progress, Rating, Segment} from "semantic-ui-react";
-import {ValueLine} from "../common";
+import {Container, Progress, Rating, Segment, Icon, Menu} from "semantic-ui-react";
+import {AdsFlag, ValueLine} from "../common";
 import {extendedPositions} from "../../const";
 import {formatCurrency, percentageToColour, valueToRating} from "../../libs/utils";
+
+import './Details.css';
 
 class DetailsView extends Component {
     render() {
@@ -11,55 +13,60 @@ class DetailsView extends Component {
         const rating = ((lineups[player.id] || {}).rating) ? ((lineups[player.id] || {}).rating).toFixed(2) : null;
         return (
             <Container fluid>
-                <Segment.Group>
-                    <Segment>
-                        <Flag name={player.nationality === 'en' ? 'gb' : player.nationality}/>
-                        <h1>{`${player.name} ${player.surname}`}</h1>
-                        <ValueLine.Group>
-                            <ValueLine label="Age" value={player.age}/>
-                            <ValueLine label="Position" value={extendedPositions[player.position].description}/>
-                            <ValueLine label="Value" value={formatCurrency(player.value)}/>
-                            {!inPlayersTeam && (
-                                <ValueLine
-                                    label={"Skill"}
-                                    value={(
-                                        <Rating
-                                            icon="star"
-                                            disabled
-                                            defaultRating={valueToRating(player.skill)}
-                                            maxRating={5}
-                                        />
-                                    )}
-                                />)}
+                <Segment>
+                    <div className="nameTitleWrapper">
+                        <div className="faceWrapper">
+                            <Icon name="user" size="big" circular/>
+                        </div>
+                        <div className="nameWrapper">
+                            <h1>{`${player.name} ${player.surname}`}</h1>
+                            <AdsFlag name={player.nationality}/>
+                        </div>
+                    </div>
+                    <ValueLine.Group>
+                        <ValueLine label="Age" value={player.age}/>
+                        <ValueLine label="Position" value={extendedPositions[player.position].description}/>
+                        <ValueLine label="Value" value={formatCurrency(player.value)}/>
+                        {!inPlayersTeam && (
+                            <ValueLine
+                                label={"Skill"}
+                                value={(
+                                    <Rating
+                                        icon="star"
+                                        disabled
+                                        defaultRating={valueToRating(player.skill)}
+                                        maxRating={5}
+                                    />
+                                )}
+                            />)}
 
-                            {inPlayersTeam && <ValueLine label={"Skill"} value={player.skill}/>}
-                            {inPlayersTeam && (
-                                <ValueLine
-                                    label={"Morale"}
-                                    value={(
-                                        <Progress
-                                            percent={player.status.morale}
-                                            color={percentageToColour(player.status.morale)}
-                                        />
-                                    )}
-                                />
-                            )}
+                        {inPlayersTeam && <ValueLine label={"Skill"} value={player.skill}/>}
+                        {inPlayersTeam && (
+                            <ValueLine
+                                label={"Morale"}
+                                value={(
+                                    <Progress
+                                        percent={player.status.morale}
+                                        color={percentageToColour(player.status.morale)}
+                                    />
+                                )}
+                            />
+                        )}
+                    </ValueLine.Group>
+                </Segment>
+                {lineups && (
+                    <Segment>
+                        <h2>Current Season</h2>
+                        <ValueLine.Group>
+                            <ValueLine label="Rate" value={rating || '-'}/>
+                            <ValueLine label="Match played" value={(lineups[player.id] || {}).played || 0}/>
+                            <ValueLine
+                                label="Goal Scored"
+                                value={(scorers[player.id] && scorers[player.id].goals) || 0}
+                            />
                         </ValueLine.Group>
                     </Segment>
-                    {lineups && (
-                        <Segment>
-                            <h2>Current Season</h2>
-                            <ValueLine.Group>
-                                <ValueLine label="Rate" value={rating || '-'}/>
-                                <ValueLine label="Match played" value={(lineups[player.id] || {}).played || 0}/>
-                                <ValueLine
-                                    label="Goal Scored"
-                                    value={(scorers[player.id] && scorers[player.id].goals) || 0}
-                                />
-                            </ValueLine.Group>
-                        </Segment>
-                    )}
-                </Segment.Group>
+                )}
             </Container>
         )
     }
