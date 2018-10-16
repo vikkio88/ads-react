d9383f9d53f43e6812e0d37db5b468dd3a92a7ca
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-10-16 23:44:47 +0100

    working on list view for messages

--- src/components/messages/List.js
+++ src/components/messages/List.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Empty} from "../common";
+import {List as SList, Icon} from "semantic-ui-react";
+
+import './List.css';
 
 class ListView extends Component {
     render() {
         const {messages} = this.props;
-        if (!messages.length) {
+        if (!messages || !messages.length) {
             return <Empty icon="mail" text="No Messages"/>;
         }
         return (
-            <pre>
-                {JSON.stringify(messages)}
-            </pre>
+            <SList relaxed divided>
+                {
+                    messages.map(m => (
+                        <SList.Item key={m.id}>
+                            <div className="messageWrapper">
+                                <div className="icon">
+                                    <Icon name={m.read ? 'envelope open' : 'envelope'} size="large"/>
+                                </div>
+                                <div className="from">{m.from}</div>
+                                <div className="subject">{m.subject}</div>
+                                <div className="date">{m.date}</div>
+                                <div className="preview">{m.message}</div>
+                            </div>
+                        </SList.Item>
+                    ))
+                }
+            </SList>
         );
     }
 }
